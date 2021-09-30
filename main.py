7e0b27522c6c50ede361ee115fd444e02d7143d8
elchicha <luis.lopezecheto@outlook.com>
2021-09-30 18:22:21 +0100

    Initial python files.

---
+++ main.py
@@ -0,0 +1,11 @@
+# Name: Unsubscribe-me
+# Date: 30-Sep-2021
+# Author: Luis Lopez-Echeto
+# Version: 0.1
+
+class UnsubscribeMe:
+    pass
+
+if __name__ == '__main__':
+    app = UnsubscribeMe()
+    app.run()
